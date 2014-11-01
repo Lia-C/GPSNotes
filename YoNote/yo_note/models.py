f2f808ea9405c61e672bc786b95346831806a2cd
shramanrc <shraman@mit.edu>
2014-11-01 10:15:10 -0400

    Model setup

--- YoNote/yo_note/models.py
+++ YoNote/yo_note/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+
+class Notes(models.Model):
+    user = models.ForeignKey(User)
+    text_body = models.CharField(max_length=200)
+    start_date = models.DateTimeField('Start Date')
+    exp_date = models.DateTimeField('Expiration Date')
